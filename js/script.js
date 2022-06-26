7799ef6cafd846d4c5e2df2963a32e046141cf51
harunbo <104387490+harunbo@users.noreply.github.com>
2022-06-26 14:13:38 +0300

    initial commit

--- js/script.js
+++ js/script.js
@@ -1,34 +1,31 @@
 
 $(document).ready(function () {
-  
-  $.getJSON("product-list.json", function(result){
+
+  $.getJSON("product-list.json", function (result) {
     showProducts(result, "Size Özel");
   });
 
-  lazyLoad();
-  
 });
 
 function showProducts(result, categoryName) {
-    
-  const products = result["responses"][0][0]["params"]["recommendedProducts"][categoryName]
-  $.each(products, function (index, product) {
-    $('.product-container').append(`<div class="product-card">
-<div class="product-image">
 
-  <img src = "${product['image']}">
+  const products = result.responses[0][0].params.recommendedProducts[categoryName]
+
+  $.each(products, function (index, product) {
+    $('.product-container').append(`<div class="product-card" data-id="${product.productId}">
+<div class="product-image">
 
 </div>
 <div class="product-info">
   <div class="product-short-description-box">
-      <p class="product-short-description">${product["name"]}</p>
+      <p class="product-short-description">${product.name}</p>
   </div>
 
-  <span class="price">${product["priceText"]}</span>
+  <span class="price">${product.priceText}</span>
   <div class="product-shipping"><i class="fa-solid fa-truck"></i>
       <p>Ücretsiz Kargo</p>
   </div>
-  ${shippingFee(product["params"]["shippingFee"])}
+  ${shippingFee(product.params.shippingFee)}
 </div>
 <div class="button-a">
 
@@ -36,102 +33,102 @@ function showProducts(result, categoryName) {
 </div>
 </div>`);
 
+
   })
+
+  lazyLoad(products);
 }
 
 
 function shippingFee(shipping) {
 
-  if (shipping === "FREE" ){
-      $(".product-shipping").css('visibility' , 'visible')
+  if (shipping === "FREE") {
+    $(".product-shipping").css('visibility', 'visible')
   }
 
   return "";
 
 }
 
-
-
-function lazyLoad() {
+function lazyLoad(items) {
   let options = {
-      root : null,
-      rootMargin : '0px',
-      threshold : 0.25
+    root: document.querySelector('.product-container'),
+    threshold: 0.5
   };
 
-  let callback = (entries, observer) => {
-      entries.forEach(entry => {
-          if(entry.isIntersecting
-              && entry.target.className === 'image' ){
-                  let imageUrl = entry.target.getAttribute('data-img');
-                  if(imageUrl){
-                      entry.target.src = imageUrl;
-                      observer.unobserve(entry.target);
-                  }
-              }
-          
-      })
-  }
-  let observer = new IntersectionObserver(callback, options);
-  let images = document.getElementsByClassName('image')
-  
-  for(let i = 0 ; i < images.length ; i++){
-      observer.observe(images.item(i))    
-  }
-  
+  let observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      const entryId = entry.target.dataset.id;
+      const item = items.find(x => x.productId === entryId);
+
+      if (item && entry.isIntersecting) {
+        const img = document.createElement('img');
+        img.src = item['image'];
+
+        entry.target.childNodes[1].appendChild(img);
+        entry.target.classList.toggle('show');
+
+        observer.unobserve(entry.target);
+      }
+
+    })
+  }, options);
+
+  let products = document.querySelectorAll('.product-card');
+
+  products.forEach((product) => {
+    observer.observe(product)
+  });
+
 }
 
 const categoryMap = {
-  "Size Özel" : "Size Özel",
-  "Tamir, Tadilat Gereçleri" : "Yapı Market & Tamirat > Tamir, Tadilat Gereçleri",
-  "Mobilya" : "Ev, Dekorasyon, Bahçe > Mobilya",
-  "Sağlık, Medikal" : "Kozmetik & Kişisel Bakım > Sağlık, Medikal",
-  "Dizüstü Bilgisayar (Laptop)" : "Bilgisayar, Tablet > Dizüstü Bilgisayar (Laptop)",
-  "Isıtma, Soğutma Sistemi" : "Beyaz Eşya & Küçük Ev Aletleri > Isıtma, Soğutma Sistemi"
+  "Size Özel": "Size Özel",
+  "Tamir, Tadilat Gereçleri": "Yapı Market & Tamirat > Tamir, Tadilat Gereçleri",
+  "Mobilya": "Ev, Dekorasyon, Bahçe > Mobilya",
+  "Sağlık, Medikal": "Kozmetik & Kişisel Bakım > Sağlık, Medikal",
+  "Dizüstü Bilgisayar (Laptop)": "Bilgisayar, Tablet > Dizüstü Bilgisayar (Laptop)",
+  "Isıtma, Soğutma Sistemi": "Beyaz Eşya & Küçük Ev Aletleri > Isıtma, Soğutma Sistemi"
 }
 
-
-const productContainers = [...document.querySelectorAll('.product-container')];
-const nxtBtn = [...document.querySelectorAll('.nxt-btn img')];
-const preBtn = [...document.querySelectorAll('.pre-btn img')];
-
-productContainers.forEach((item, i) => {
-  let containerDimensions = item.getBoundingClientRect();
-  let containerWidth = containerDimensions.width;
-
-  nxtBtn[i].addEventListener('click', () => {
-    item.scrollLeft += 550;
-  })
-
-  preBtn[i].addEventListener('click', () => {
-    item.scrollLeft -= 550;
-  })
-})
-
 $(document).ready(function () {
+
+  const productContainers = [...document.querySelectorAll('.product-container')];
+
+  $('.nxt-btn img').click(function () {
+    productContainers.forEach((item, i) => {
+      item.scrollLeft += 550;
+    })
+  });
+
+  $('.pre-btn img').click(function () {
+    productContainers.forEach((item, i) => {
+      item.scrollLeft -= 550;
+    })
+  });
   
   let timer = null;
 
   $('.product-container').on('click', '.button-a', function () {
 
     if (timer) {
-    
-      clearTimeout (timer);
+
+      clearTimeout(timer);
       timer = null;
-  
+
     }
 
-    if($('.alert').hasClass("show")){
+    if ($('.alert').hasClass("show")) {
 
       $('.alert').removeClass("showAlert")
       $('.alert').removeClass("show")
 
-     setTimeout(function () {
+      setTimeout(function () {
         $('.alert').addClass("show")
         $('.alert').addClass("showAlert")
       }, 100);
-      
-    }else{
+
+    } else {
 
       $('.alert').addClass("show")
       $('.alert').removeClass("hide")
@@ -140,17 +137,17 @@ $(document).ready(function () {
 
     timer = setTimeout(function () {
       waitFunction();
-      clearTimeout (timer);
+      clearTimeout(timer);
       timer = null;
-    },2000);
+    }, 2000);
 
-    function waitFunction () {
+    function waitFunction() {
 
       $('.alert').removeClass("show");
       $('.alert').addClass("hide");
-      
-  }
-    
+
+    }
+
   });
 
   $('.close-btn').click(function () {
@@ -158,29 +155,29 @@ $(document).ready(function () {
     $('.alert').addClass("hide");
   });
 
+  $('li button').click(function () {
+
+    $(".product-container").empty()
   
+    $('li button').each(function () {
+      $(this).removeClass('selectedclass')
+      $(this).closest("li").removeClass('selectedclass')
+    });
+  
+    $(this).addClass('selectedclass');
+    $(this).closest("li").addClass('selectedclass');
+  
+  
+    let categoryName = $(this).text();
+  
+    $.getJSON("product-list.json", function (result) {
+      showProducts(result, categoryMap[categoryName]);
+    });
+  });
 
 });
 
-$('li button').click(function () {
 
-  $(".product-container").empty()
-
-  $('li button').each(function () {
-    $(this).removeClass('selectedclass')
-    $(this).closest("li").removeClass('selectedclass')
-  });
-
-  $(this).addClass('selectedclass');
-  $(this).closest("li").addClass('selectedclass');
-
-
-  let categoryName = $(this).text();
-
-  $.getJSON("product-list.json", function(result){
-    showProducts(result, categoryMap[categoryName]);
-  });
-});
 
 
 
